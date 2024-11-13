de62638f8944cd897837791cfffd7c25ed1222fa
Carlos Torres <carltorres935@gmail.com>
2024-11-13 16:22:10 -0600

    final

--- webapp/assets/js/main.js
+++ webapp/assets/js/main.js
@@ -39,7 +39,7 @@ async function enviarDatos() {
         // Actualizar el texto del nivel de fatiga
         const fatigueLevelText = document.getElementById("fatigue-level-text");
         fatigueLevelText.textContent = probabilidadFatiga >= 0.5
-            ? "Alerta: Alto nivel de fatiga detectado"
+            ? "Alerta: Nivel preocupante de fatiga detectado"
             : "Nivel de fatiga dentro de límites normales";
 
         if (probabilidadFatiga >= 0.7) {
@@ -67,10 +67,23 @@ async function enviarDatos() {
         document.getElementById("recomendacion-texto").textContent = mensajeRecomendacion;
         document.getElementById("recomendacion-icono").innerHTML = iconoRecomendacion;
 
+        // contenedor circular
+        const tiempoEjercicio = datos["Tiempo de Ejercicio (min)"];
+        const objetivo = 80;
+        const progreso = Math.min((tiempoEjercicio / objetivo) * 100, 100).toFixed(1);
+
+        // Actualizar el marcador circular
+        const marcador = document.querySelector(".marcador-circular");
+        marcador.style.background = `conic-gradient(#FA7E61 ${progreso * 3.6}deg, #F5EFED ${progreso * 3.6}deg)`;
+
+        // Actualizar el texto de porcentaje en el centro del marcador
+        const textoPorcentaje = document.getElementById("porcentaje-texto");
+        textoPorcentaje.textContent = `${progreso}%`;
+
     } catch (error) {
         console.error("Error en la solicitud:", error);
         document.getElementById('resultado').textContent = "Error en la solicitud, intente nuevamente.";
-    }
+    }   
 }
 
 
